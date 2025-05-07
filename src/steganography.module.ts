df4dfc7a71fab610f80c80810f27c2503a69fd8c
duongduke <duongduke1708@gmail.com>
2025-05-07 16:04:18 +0700

    tối ưu, tinh gọn cấu trúc nestJS

--- src/steganography.module.ts
+++ src/steganography.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SteganographyController } from './steganography.controller';
+import { SteganographyService } from './steganography.service';
 
-@Module({})
+@Module({
+  controllers: [SteganographyController],
+  providers: [SteganographyService],
+  exports: [SteganographyService]
+})
 export class SteganographyModule {}
